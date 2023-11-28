70f3bd27183239ea6b81a4c2658b634f00976e59
TataQuispe <152334280+TataQuispe@users.noreply.github.com>
2023-11-28 17:40:47 -0400

    pagina

---
+++ stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    {id: 1, nombre: "Revista 15/11/2023", tipo: "revista", cantidad: 1, desc: "ANUEL TENDRÁ UN HIJO", precio: 16, img: "Billboard Shop/4cover_Rosalia_RauwAlejandro_HIGHRES_002_400x.webp"},
+    {id: 2, nombre: "Revista 08/11/2023", tipo: "revista", cantidad: 1, desc: "MALUMA SE RAPARÁ", precio: 12, img: 'Billboard Shop/4cover_Rosalia_RauwAlejandro_HIGHRES_002_400x.webp'},
+    {id: 3, nombre: "Revista 01/11/2023", tipo: "revista", cantidad: 1, desc: "SHAKIRA SE CASA", precio: 12, img: './img/Billboard Shop/8cover_MarenMorris_HIGHRES_400x.webp'},
+    {id: 4, nombre: "Revista 25/10/2023", tipo: "revista", cantidad: 1, desc: "BAD BUNNY GAY?", precio: 14, img:'.'},
+   ]
